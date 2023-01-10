c9238c0248355b67d25c905192b95e8c46494fd7
Joancel Castillo <powermetal_5000@hotmail.com>
2023-01-10 13:58:18 -0400

    cambios

---
+++ Exercise2Hour/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise2Hour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = DateTime.Now.ToString("h:mm:ss tt");
+            Console.WriteLine("La hora es: {0}", time);
+        }
+    }
+}
